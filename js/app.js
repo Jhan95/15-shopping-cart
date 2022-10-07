f290381809ed4dc1546be950162380ebb6a0ee38
Jhan Vásquez <jhanvasquez95@gmail.com>
2022-10-06 20:07:52 -0500

    Se creo objeto dinamico para agregar los detalles del prodcuto

--- js/app.js
+++ js/app.js
@@ -16,7 +16,25 @@ function agregarCurso(e) {
      e.preventDefault();
 
      if( e.target.classList.contains('agregar-carrito') ) {
-          console.log('Agregando al carrito');
+          const cursoSeleccionado = e.target.parentElement.parentElement;
+
+          leerDatosCursos(cursoSeleccionado);
      }
+
+}
+
+function leerDatosCursos(curso) {
+     console.log(curso);
+
+     const infoCurso = {
+          imagen: curso.querySelector('img').src,
+          titulo: curso.querySelector('h4').textContent,
+          precio: curso.querySelector('.precio span').textContent,
+          id: curso.querySelector('a').getAttribute('data-id'),
+          cantidad: 1,
+
+     }
+
+     console.log(infoCurso);
 }
 
