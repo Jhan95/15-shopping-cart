6f8ab84bbb831bbca75ddf8f7efe672edd1c5492
Jhan Vasquez <jhanvasquez95@gmail.com>
2023-01-12 14:26:23 -0500

    Funcionalidad de boton agregar

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//Variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+eventListeners();
+function eventListeners(){
+     listaCursos.addEventListener('click', agregarCurso);
+}
+
+//Funnctions
+function agregarCurso(e){
+     e.preventDefault();
+     if(e.target.classList.contains("agregar-carrito")){
+          console.log("agregando");
+     }
+}
