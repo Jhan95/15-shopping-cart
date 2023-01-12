556190fdf9408166835f52bd083d0c8b8bed1a0f
Jhan Vasquez <jhanvasquez95@gmail.com>
2023-01-12 14:57:23 -0500

    Avances de carrito

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
+let articulosCarrito = [];
+
 eventListeners();
 function eventListeners(){
      listaCursos.addEventListener('click', agregarCurso);
@@ -13,6 +15,38 @@ function eventListeners(){
 function agregarCurso(e){
      e.preventDefault();
      if(e.target.classList.contains("agregar-carrito")){
-          console.log("agregando");
+          const cursoSeleccionado = e.target.parentElement.parentElement;
+
+          leerDatosCurso(cursoSeleccionado);
      }
+}
+
+
+function leerDatosCurso(curso) {
+     console.log(curso);
+
+     const infoCurso = {
+          imagen: curso.querySelector('img').src,
+          titulo: curso.querySelector('h4').textContent,
+          precio: curso.querySelector('.precio span').textContent,
+          id: curso.querySelector('a').getAttribute('data-id'),
+          cantidad: 1,
+     }
+
+     articulosCarrito = [...articulosCarrito, infoCurso];
+
+     console.log(articulosCarrito);
+
+     carritoHTML();
+}
+
+function carritoHTML() {
+     articulosCarrito.forEach( curso => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+          <td>
+               ${curso.titulo}
+          </td>
+          `
+     })
 }
