ca8dee336d603a5c5bd24195f1d392c111d87702
Jhan Vasquez <jhanvasquez95@gmail.com>
2023-01-13 09:30:35 -0500

    primer version

--- js/app.js
+++ js/app.js
@@ -9,6 +9,14 @@ let articulosCarrito = [];
 eventListeners();
 function eventListeners(){
      listaCursos.addEventListener('click', agregarCurso);
+
+     carrito.addEventListener('click', eliminarCurso);
+
+     vaciarCarritoBtn.addEventListener('click', () => {
+          articulosCarrito = [];
+
+          limpiarHTML();
+     })
 }
 
 //Funnctions
@@ -18,6 +26,18 @@ function agregarCurso(e){
           const cursoSeleccionado = e.target.parentElement.parentElement;
 
           leerDatosCurso(cursoSeleccionado);
+     }
+}
+
+function eliminarCurso(e){
+     if(e.target.classList.contains('borrar-curso')){
+          const cursoId = e.target.getAttribute('data-id');
+
+          articulosCarrito = articulosCarrito.filter( curso => curso.id !== cursoId);
+
+          carritoHTML();
+
+          console.log(articulosCarrito);
      }
 }
 
