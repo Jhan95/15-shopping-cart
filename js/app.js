3b8c3ce74c29f1627fdfb1a1da52ef0ab109bfdf
Jhan Vasquez <jhanvasquez95@gmail.com>
2023-01-14 10:10:29 -0500

    informacion agregada al tbody

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const contenedorCarrito = document.querySelector('#lista-carrito');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
+let articulosCarrito = [];
+
 eventListeners();
 function eventListeners(){
      listaCursos.addEventListener('click', agregarCurso);
@@ -20,7 +22,6 @@ function agregarCurso(e){
 }
 
 function leerDatosCursos(curso){
-     console.log(curso);
 
      const infoCurso = {
           imagen: curso.querySelector('img').src,
@@ -30,6 +31,33 @@ function leerDatosCursos(curso){
           cantidad: 1,
      }
 
+     articulosCarrito = [...articulosCarrito, infoCurso];
 
-     console.log(infoCurso);
+     console.log(articulosCarrito);
+
+     carritoHTML();
+}
+
+function carritoHTML(){
+     limpiarHTML();
+
+     articulosCarrito.forEach( curso => {
+          const {imagen, titulo, precio, id, cantidad} = curso;
+          const row = document.createElement('tr');
+          row.innerHTML = `
+          <td><img src="${imagen}" width="100"></td>
+          <td>${titulo}</td>
+          <td>${precio}</td>
+          <td>${cantidad}</td>
+          <td><a href="#" class="borrar-curso" dadta-id="${id}"> X <a/></td>
+          `;
+
+          contenedorCarrito.appendChild(row);
+     })
+}
+
+function limpiarHTML(){
+     while(contenedorCarrito.firstChild){
+          contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+     }
 }
