4f1087701f9ae0f65b77f6068003db5e23eff79e
Jhan Vasquez <jhanvasquez95@gmail.com>
2023-01-16 10:53:09 -0500

    Agregando cursos en el html y boton de limpiar para que no se repitan

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
+let articulosCarrito = [];
+
 eventListeners();
 function eventListeners(){
      listaCursos.addEventListener('click', agregarCurso);
@@ -29,5 +31,37 @@ function leerDatosCurso(curso){
           cantidad: 1,
      }
 
-     console.log(infoCurso);
+     articulosCarrito = [...articulosCarrito, infoCurso];
+
+     console.log(articulosCarrito);
+
+     carritoHTML();
+}
+
+
+function carritoHTML(){
+
+     limpiarHTML();
+
+     articulosCarrito.forEach( curso => {
+          const {imagen, titulo, precio, cantidad, id} = curso;
+          const row = document.createElement('tr');
+          row.innerHTML = `
+          <td><img src="${imagen}" width="100"></td>
+          <td>${titulo}</td>
+          <td>${precio}</td>
+          <td>${cantidad}</td>
+          <td><a href="#" class="borrar-curso" data-id="${id}"> X <a/></td>
+     
+          `;
+
+          contenedorCarrito.appendChild(row);
+     });
+}
+
+function limpiarHTML(){
+
+     while(contenedorCarrito.firstChild){
+          contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+     }
 }
