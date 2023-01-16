d0b0a2d8fe442cbab1b750333f175975068dbf2e
Jhan Vasquez <jhanvasquez95@gmail.com>
2023-01-16 07:38:47 -0500

    variables y Funcion para agregar cursos

--- js/app.js
+++ js/app.js
@@ -3,83 +3,15 @@ const contenedorCarrito = document.querySelector('#lista-carrito');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
-let articulosCarrito = [];
-
 eventListeners();
 function eventListeners(){
      listaCursos.addEventListener('click', agregarCurso);
-
-     carrito.addEventListener('click', eliminarCurso);
 }
 
 
 function agregarCurso(e){
      e.preventDefault();
-
      if(e.target.classList.contains('agregar-carrito')){
-          const cursoSeleccionado = e.target.parentElement.parentElement;
-
-          leerDatosCursos(cursoSeleccionado);
-     }
-}
-
-function eliminarCurso(e){
-     if(e.target.classList.contains('borrar-curso')){
-          console.log(e.target.getAttribute('data-id'));
-     }
-}
-
-
-function leerDatosCursos(curso){
-
-     const infoCurso = {
-          imagen: curso.querySelector('img').src,
-          titulo: curso.querySelector('h4').textContent,
-          precio: curso.querySelector('.precio span').textContent,
-          id: curso.querySelector('a').getAttribute('data-id'),
-          cantidad: 1,
-     }
-
-     const existe = articulosCarrito.some( curso => curso.id === infoCurso.id);
-     if(existe){
-          const cursos = articulosCarrito.map( curso => {
-               if (curso.id === infoCurso.id){
-                    curso.cantidad++;
-                    return curso
-               } else{
-                    return curso;
-               }
-          })
-          articulosCarrito = [...cursos]
-     } else{
-          articulosCarrito = [...articulosCarrito, infoCurso];
-     }
-
-
-
-     carritoHTML();
-}
-
-function carritoHTML(){
-     limpiarHTML();
-
-     articulosCarrito.forEach( curso => {
-          const {imagen, titulo, precio, id, cantidad} = curso;
-          const row = document.createElement('tr');
-          row.innerHTML = `
-          <td><img src="${imagen}" width="100"></td>
-          <td>${titulo}</td>
-          <td>${precio}</td>
-          <td>${cantidad}</td>
-          <td><a href="#" class="borrar-curso" dadta-id="${id}"> X <a/></td>
-          `;
-
-          contenedorCarrito.appendChild(row);
-     })
-}
-
-function limpiarHTML(){
-     while(contenedorCarrito.firstChild){
-          contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+          console.log('agregando');
      }
 }
