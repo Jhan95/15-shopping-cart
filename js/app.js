0cea368bcb1d15a53b972a2cd3f9a2cbbad51408
Jhan Vasquez <jhanvasquez95@gmail.com>
2023-01-17 07:37:58 -0500

    variables y funciones para agregar y leer cursos

--- js/app.js
+++ js/app.js
@@ -3,56 +3,35 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
-let articulosCarrito = [];
-
 
 eventListeners();
 function eventListeners(){
      listaCursos.addEventListener('click', agregarCurso);
+
 }
+
 
 function agregarCurso(e){
      e.preventDefault();
+
      if(e.target.classList.contains('agregar-carrito')){
+
           const cursoSeleccionado = e.target.parentElement.parentElement;
 
           leerDatosCurso(cursoSeleccionado);
      }
 }
 
+
 function leerDatosCurso(curso){
      console.log(curso);
 
-     const infoCursos = {
+     const infoCurso = {
           imagen: curso.querySelector('img').src,
           titulo: curso.querySelector('h4').textContent,
           precio: curso.querySelector('.precio span').textContent,
           id: curso.querySelector('a').getAttribute('data-id'),
           cantidad: 1,
      }
-
-     articulosCarrito = [...articulosCarrito, infoCursos];
-     console.log(articulosCarrito);
-}
-
-function carritoHTML(){
-
-     limpiarHTML();
-
-     articulosCarrito.forEach( curso => {
-          const row = document.createElement('tr');
-          row.innerHTML = `
-          
-          <td>${curso.titulo}</td>
-          `;
-
-          contenedorCarrito.appendChild(row);
-     });
-}
-
-function limpiarHTML(){
-
-     while(contenedorCarrito.firstChild){
-          contenedorCarrito.removeChild(contenedorCarrito.firstElementChild);
-     }
+     console.log(infoCurso);
 }
