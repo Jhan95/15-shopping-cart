9fd7fbd0b8c75c3e243e8940c0fd15a0280dc644
Jhan Vasquez <jhanvasquez95@gmail.com>
2023-01-17 08:37:11 -0500

    funcionalidad de boton eliminar y vaciar carrito

--- js/app.js
+++ js/app.js
@@ -10,6 +10,14 @@ eventListeners();
 function eventListeners(){
      listaCursos.addEventListener('click', agregarCurso);
 
+     carrito.addEventListener('click', eliminarCurso);
+
+     vaciarCarritoBtn.addEventListener('click', () => {
+          articulosCarrito = [];
+
+          limpiarHTML();
+     })
+
 }
 
 
@@ -23,6 +31,18 @@ function agregarCurso(e){
           leerDatosCurso(cursoSeleccionado);
      }
 }
+
+
+function eliminarCurso(e){{
+
+     if(e.target.classList.contains('borrar-curso')){
+          const cursoId = e.target.getAttribute('data-id');
+
+          articulosCarrito = articulosCarrito.filter( curso => curso.id !== cursoId);
+
+          carritoHTML();
+     }
+}}
 
 
 function leerDatosCurso(curso){
