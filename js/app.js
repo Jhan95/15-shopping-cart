a9a3bb5b33abd485e9087fe9cc2370cbcd4b70a9
Jhan Vasquez <jhanvasquez95@gmail.com>
2023-01-17 10:52:07 -0500

    contenido del carrito al HTML y funcion para limpiar los cursos repetidos

--- js/app.js
+++ js/app.js
@@ -47,13 +47,14 @@ function carritoHTML(){
 
      articulosCarrito.forEach( curso => {
 
-          const { imagen, titulo, precio, cantidad} = curso;
+          const { imagen, titulo, precio, cantidad, id} = curso;
           const row = document.createElement('tr');
           row.innerHTML = `
           <td><img src="${imagen}" width="100"></td>
           <td>${titulo}</td>
           <td>${precio}</td>
           <td>${cantidad}</td>
+          <td><a href="#" class="borrar-curso" data-id="${id}"> X </a></td>
           `;
 
           contenedorCarrito.appendChild(row);
